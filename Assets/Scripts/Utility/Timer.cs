12022624c0558fa4c20fcc125bdd1257f944d0a3
AliEnverSevgi <alienversevgi@hotmail.com>
2022-07-07 15:16:10 +0300

    Utility scripts added

---
+++ Assets/Scripts/Utility/Timer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Utility
+{
+    public class Timer : Singleton<Timer>
+    {
+        public Coroutine StartTimer(float time, Action action)
+        {
+            return StartCoroutine(WaitAndExecute(time, action));
+        }
+
+        public void Stop(Coroutine timer)
+        {
+            if (timer != null)
+                StopCoroutine(timer);
+        }
+
+        public void StopAllCoroutine()
+        {
+            StopAllCoroutines();
+        }
+
+        private IEnumerator WaitAndExecute(float time, Action action)
+        {
+            yield return new WaitForSecondsRealtime(time);
+            action?.Invoke();
+        }
+    }
+}
